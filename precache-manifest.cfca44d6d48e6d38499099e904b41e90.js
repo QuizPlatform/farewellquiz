2f5e8525f2feaf038a16f15f5c55e166b19cc6d0
= <awesomepranshu@gmail.com>
2020-10-16 15:58:03 +0530

    Updates

---
+++ precache-manifest.cfca44d6d48e6d38499099e904b41e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16bd804bfdac85ff50b6e3b09492e265",
+    "url": "/farewellquiz/index.html"
+  },
+  {
+    "revision": "ee6967d70083817c555f",
+    "url": "/farewellquiz/static/css/main.bc31f057.chunk.css"
+  },
+  {
+    "revision": "573fa52770aa0db9d883",
+    "url": "/farewellquiz/static/js/2.507c5705.chunk.js"
+  },
+  {
+    "revision": "88c7adc58448a2661ef1f8f7fb418351",
+    "url": "/farewellquiz/static/js/2.507c5705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee6967d70083817c555f",
+    "url": "/farewellquiz/static/js/main.9f0bf0c1.chunk.js"
+  },
+  {
+    "revision": "645258989903cd152d72",
+    "url": "/farewellquiz/static/js/runtime-main.2dc2903a.js"
+  }
+]);
